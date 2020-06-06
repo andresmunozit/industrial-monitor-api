7e9d8d8c5df7c36381ba62e9ae330cf380d6232d
Andres Munoz <am@andresmunozit.com>
2020-06-06 09:09:06 -0500

    Implementation of the login endpoint

--- src/routers/api/v1.js
+++ src/routers/api/v1.js
@@ -1,11 +1,28 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 
 const adminRouter = require('./v1/admin');
 const userRouter = require('./v1/user');
 const telemetryRouter = require('./v1/user');
 
+const User = require('../../models/user');
+
 router.use('/admin', adminRouter);
 router.use('/user', userRouter);
 router.use('/telemetry', telemetryRouter);
+
+// Login middleware temporary
+router.post('/login', async (req, res) => { // Test username and password and issue a token
+    try {
+        const { email, password } = req.body;
+        const user = await User.getUserByCredentials(email, password);
+        if(!user) return res.status(403).json({msg: 'Wrong credentials'});
+        const token = await user.createToken();
+
+        res.json({user, token});
+    } catch {
+        res.status(500).json();  
+    };
+});
 
 module.exports = router;
