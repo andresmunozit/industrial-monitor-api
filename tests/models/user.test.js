3dcb1194607a86ea0ac826ea0a600df9e6a650e8
Andres Munoz <am@andresmunozit.com>
2020-06-07 12:29:28 -0500

    Test token creation, token verification and email validation in User model

--- tests/models/user.test.js
+++ tests/models/user.test.js
@@ -27,7 +27,7 @@ test('Should return null whith wrong credentials', async () => {
     expect(user).toBe(null);
 });
 
-test('Should emit a token', async () => {
+test('Should create a token', async () => {
     const user = await new User(userOne).save();
     const token = await user.createToken();
 
@@ -35,7 +35,48 @@ test('Should emit a token', async () => {
     expect(token).toBe(dbUser.tokens[0].token);
 });
 
-test.todo('Should verify the email address');
+test('Should verify the email address', async () =>{
+    userOne.email = 'wrong email';
+    try{
+        const user = await new User(userOne).save();
+    } catch (error){
+        expect(error.message).toBe('User validation failed: email: wrong email is not a valid email');
+    };
+});
+
+test('Should verify if a token is in the tokens array', async () => {
+    const user = await User.findById(userTwo._id);
+    const token = await user.createToken();
+
+    expect(user.validToken(token)).toBe(true);
+    expect(user.validToken('wrong token')).toBe(false);
+    expect(user.validToken('')).toBe(false);
+});
+
+test('Should delete a token from user tokens', async () => {
+    const user = await User.findById(userTwo._id);
+    const token1 = await user.createToken();
+    const token2 = await user.createToken();
+    const token3 = await user.createToken();
+    const token4 = await user.createToken();
+    expect(user.tokens.length).toBe(4);
+
+    expect(user.validToken(token3)).toBe(true);
+
+    await user.deleteToken(token3);
+    expect(user.validToken(token3)).toBe(false);
+    expect(user.tokens.length).toBe(3);
+});
+
+test('Should throw an error if a deletion of a non-existent token is attempted', async () => {
+    const user = await User.findById(userTwo._id);
+    const token = await user.createToken();
+    try{
+        await user.deleteToken('wrong token');
+    }catch(error){
+        expect(error).toBe('Token doesn\'t exist');
+    };
+});
 
 afterAll(() => {
     return mongoose.connection.close();
