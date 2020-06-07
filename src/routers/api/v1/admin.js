74c97629cac987b0dbf4e20e2b7ace5332a4a672
Andres Munoz <am@andresmunozit.com>
2020-06-07 12:25:52 -0500

    Configure auth middleware to be used in all /api/v1/admin/ endpoints

--- src/routers/api/v1/admin.js
+++ src/routers/api/v1/admin.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const usersRouter = require('./admin/users');
 const devicesRouter = require('./admin/devices');
+const auth = require('../../../middleware/auth');
+router.use(auth);  // All /api/v1/admin/ endpoints are authenticated
 
 router.use('/users', usersRouter);
 router.use('/devices', devicesRouter);
