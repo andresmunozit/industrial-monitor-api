eb360359626642d8777cd59873be7add758860e1
Andres Munoz <am@andresmunozit.com>
2020-06-07 12:30:55 -0500

    Implementation of tests for logout endpoint

--- tests/routers/api/v1.test.js
+++ tests/routers/api/v1.test.js
@@ -6,6 +6,7 @@ const { userTwo, setupDatabase } = require('../../fixtures/db');
 
 beforeEach( setupDatabase );
 
+// Login tests
 test('Should authenticate a user', async () => {
     const user = userTwo;
     const response = await request(app)
@@ -46,6 +47,57 @@ test('Should not authenticate a user with wrong password', async () => {
         .expect(403);
 
     expect(response.body.msg).toBe('Wrong credentials');
+});
+
+// Logout tests
+test('Should logout a logged in user', async () => {
+    const user = userTwo;
+
+    const loginResponse = await request(app)
+        .post('/api/v1/login')
+        .send({
+            email: user.email,
+            password: user.password
+        })
+        .expect(200);
+
+    const token = loginResponse.body.token;
+    const userDBBeforeLogout = await User.findById(user._id);
+    expect(userDBBeforeLogout.validToken(token)).toBe(true);
+
+    const logoutResponse = await request(app)
+        .get('/api/v1/logout')
+        .set('Authorization', 'Bearer ' + token)
+        .send()
+        .expect(200);
+
+    const userDBAfterLogout = await User.findById(user._id);
+    expect(userDBAfterLogout.validToken(token)).toBe(false);
+});
+
+test('Should receive "Not authorized" after logout', async () => {
+    const user = userTwo;
+    const loginResponse = await request(app)
+        .post('/api/v1/login')
+        .send({
+            email: user.email,
+            password: user.password
+        })
+        .expect(200);
+    const token = loginResponse.body.token;    
+
+    const logoutResponse = await request(app)
+        .get('/api/v1/logout')
+        .set('Authorization', 'Bearer ' + token)
+        .send()
+        .expect(200);
+
+    const notAuthorizedResponse = await request(app)
+        .get('/api/v1/logout')
+        .set('Authorization', 'Bearer ' + token)
+        .send()
+        .expect(403);
+    expect(notAuthorizedResponse.body.msg).toBe('Not authorized');
 });
 
 afterAll(() => {
