3f74f30a6649c83dd3bec28584d8ef7e31da8abe
Andres Munoz <am@andresmunozit.com>
2020-06-25 10:13:06 -0500

    Creation of the resetPassword email template

--- src/helpers/email/emailTemplates.js
+++ src/helpers/email/emailTemplates.js
@@ -2,10 +2,20 @@ const welcome = (email, name, temporaryPassword) => {
     return {
         to: email,
         from: process.env.EMAIL_SENDER_ADDRESS,
-        subject: 'Welcome to Industrial Monitor API',
+        subject: 'Industrial Monitor API - Welcome!',
         text: `Hello ${name}\n\nAn account has been created for you to access to the application. A temporary password has been generated, you need to change this password in order to access to the application.\n\nTemporary password: ${temporaryPassword}\n\nYou can login to the application in the following link:\n\n${process.env.FRONTEND_URL}/login`,
         // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     };
 };
 
-module.exports = { welcome };
+const resetPassword = (email, name, temporaryPassword) => {
+    return {
+        to: email,
+        from: process.env.EMAIL_SENDER_ADDRESS,
+        subject: 'Industrial Monitor API - Your password has been reset',
+        text: `Hello ${name}\n\nAn administrator has reset your password. A temporary password has been generated, you need to change this password in order to access to the application.\n\nTemporary password: ${temporaryPassword}\n\nYou can login to the application in the following link:\n\n${process.env.FRONTEND_URL}/login`,
+        // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+};
+
+module.exports = { welcome, resetPassword };
