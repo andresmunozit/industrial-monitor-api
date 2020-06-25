1999f179c83bbc348d9c426579bb0296f3dfefa4
Andres Munoz <am@andresmunozit.com>
2020-06-25 09:24:16 -0500

    Fix dot character adition to the temporary password sent to the new users

--- src/helpers/email/emailTemplates.js
+++ src/helpers/email/emailTemplates.js
@@ -3,7 +3,7 @@ const welcome = (email, name, temporaryPassword) => {
         to: email,
         from: process.env.EMAIL_SENDER_ADDRESS,
         subject: 'Welcome to Industrial Monitor API',
-        text: `Hello ${name}\n\nAn account has been created for you to access to the application. A temporary password has been generated, you need to change this password in order to access to the application.\n\nTemporary password: ${temporaryPassword}.\n\nYou can login to the application in the following link:\n\n${process.env.FRONTEND_URL}/login`,
+        text: `Hello ${name}\n\nAn account has been created for you to access to the application. A temporary password has been generated, you need to change this password in order to access to the application.\n\nTemporary password: ${temporaryPassword}\n\nYou can login to the application in the following link:\n\n${process.env.FRONTEND_URL}/login`,
         // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     };
 };
