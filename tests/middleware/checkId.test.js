8ebc88deb0b50fca71f385ffedc69116976b89d2
Andres Munoz <am@andresmunozit.com>
2020-06-26 10:14:57 -0500

    Implementation of checkId middleware test suite

--- tests/middleware/checkId.test.js
+++ tests/middleware/checkId.test.js
@@ -1,1 +1,31 @@
-test.todo('Implement checkId middleware validation suite');
+const request = require('supertest');
+const app = require('../../src/app');
+const checkId = require('../../src/middleware/checkId');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+test('Should return status 404 if a wrong id format is sent into the id parameter', async () => {
+    app.get('/checkIdTest1/:id', checkId, (req, res) => {
+        res.json('ok');
+    });
+
+    const wrongId  = '1234567890asdfghjk';
+    const response = await request(app)
+        .get(`/checkIdTest1/${wrongId}`)
+        .send()
+        .expect(404);
+});
+
+test('Should call to next, and return status 200 if a correct format id is specified', async () => {
+    app.get('/checkIdTest2/:id', checkId, (req, res) => {
+        res.json('ok');
+    });
+
+    const correctId = new ObjectId();
+
+    const response = await request(app)
+        .get(`/checkIdTest2/${correctId}`)
+        .send()
+        .expect(200);
+
+    expect(response.body).toBe('ok');
+});
