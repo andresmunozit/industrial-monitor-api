8eb6fdde1f53215be5524bb5e000da3378338d93
Andres Munoz <am@andresmunozit.com>
2020-06-26 09:45:27 -0500

    Add error throwing to allowedBodyParams middleware, when no parameters are provided or the parameters contain spaces or special characters

--- src/middleware/allowedBodyParams.js
+++ src/middleware/allowedBodyParams.js
@@ -1,11 +1,14 @@
 const allowedBodyParams = (...allowedParams) => {
+    if(allowedParams.length === 0) throw 'You must provide at least one allowed parameter';
+    if(allowedParams.join('').includes(' ')) throw 'Allowed body parameters cannot contain spaces';
+    if(allowedParams.join('').match(/[$-/:-?{-~!"^_`\[\]]/)) throw 'Allowed body parameters cannot contain special characters';
+
     return (req, res, next) => {
-        if(!allowedParams) throw 'You must provide at least one allowed parameter';
 
         const params = Object.keys(req.body);
 
         const notAllowedParams = params.filter( param => !allowedParams.includes(param) );
-        if(notAllowedParams.length) return res.status(400).json({msg: `Illegal body parameter(s): ${notAllowedParams}`});
+        if(notAllowedParams.length) return res.status(400).json({msg: `Illegal body parameter(s): ${notAllowedParams.join(', ')}`});
 
         req.allowedBodyParams = allowedParams;
 
