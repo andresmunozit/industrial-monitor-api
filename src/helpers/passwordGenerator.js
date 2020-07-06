246a67cad29a2e0ba825d402e64541ac70d6e0c7
Andres Munoz <am@andresmunozit.com>
2020-07-05 23:36:06 -0500

    Add error control to generatePassword module

--- src/helpers/passwordGenerator.js
+++ src/helpers/passwordGenerator.js
@@ -1,6 +1,9 @@
 const randomize = require('randomatic');
 
 const generatePassword = ( config = '*', length = 16 ) => {
+    if( !['a','A', '0', '!', '*'].includes(config)) throw 'Invalid config string. Only "a" (lowercase), "A" (uppercase), "0" (numeric), "!" (special) and "*" (all above) are allowed characters';
+    if( !Number(length) && Number(length) !== 0 ) throw 'Length parameter must be a number';
+    if( length < 1 || length > 32 ) throw 'Password length parameter must be greater than 0 and less than 33';
     return randomize(config, length);
 };
 
