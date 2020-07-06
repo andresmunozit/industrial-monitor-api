caa792c227c8e19e6760607313e51fe8624221ee
Andres Munoz <am@andresmunozit.com>
2020-07-06 14:38:51 -0500

    Correction of error handling of the allowedQueryParams middlware

--- src/middleware/allowedQueryParams.js
+++ src/middleware/allowedQueryParams.js
@@ -1,12 +1,9 @@
 const allowedQueryParams = (...allowedParams) => {
+    if(allowedParams.length === 0) throw 'You must provide at least one allowed parameter';
     return (req, res, next) => {
-        if(!allowedParams) throw 'You must provide at least one allowed parameter';
-
         const params = Object.keys(req.query);
-
         const notAllowedParams = params.filter( param => !allowedParams.includes(param) );
         if(notAllowedParams.length) return res.status(400).json({msg: `Illegal query parameter(s): ${notAllowedParams.join(', ')}.`});
-
         next();
     };
 };
