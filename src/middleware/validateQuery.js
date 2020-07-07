34a18b584b3c6655b6e65a1f1e7529fc34d3377c
Andres Munoz <am@andresmunozit.com>
2020-07-07 11:39:14 -0500

    Improve skip validation error message

--- src/middleware/validateQuery.js
+++ src/middleware/validateQuery.js
@@ -44,8 +44,8 @@ const validateLimit = (limit, maxLimit) => {
 const validateSkip = skip => {
     if (!skip) return 0;
     if (Array.isArray(skip)) return {error: `A single value is supported for "skip" parameter, instead "${skip.join(', ')}" were provided.`};
-    if (!parseInt(Number(skip))) return {error: `"skip" must be a positive integer, "${skip}" received instead.`};
-    if (skip < 0 || skip > Math.pow(2, 31)) return {error: `"skip" must be an integer between 0 and 2^31, "${skip}" received instead.`};
+    if (!parseInt(Number(skip))) return {error: `"skip" must be a positive integer, "${skip}" value was received instead.`};
+    if (skip < 0 || skip > Math.pow(2, 31)) return {error: `"skip" must be an integer between 0 and 2^31, "${skip}" value was received instead.`};
     return parseInt(skip);
 };
 
